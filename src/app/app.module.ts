ee932db27454ffcf72bc6c77dddb2bd3cbd60aa5
700blade007 <dante.nero.1994@gmail.com>
2018-11-18 02:52:46 +0530

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ChartComponent } from './chart/chart.component';
 import { ChooseColumnComponent } from './choose-column/choose-column.component';
+import { UploadModule } from './upload/upload.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ChooseColumnComponent } from './choose-column/choose-column.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    UploadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
